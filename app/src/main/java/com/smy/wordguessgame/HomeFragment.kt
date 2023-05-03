cb09d144f1d7ff964783f54d750ba0d2654596c8
yarin <yarin1038@gmail.com>
2023-05-03 14:53:23 +0300

    added functional buttons in main menu

--- app/src/main/java/com/smy/wordguessgame/HomeFragment.kt
+++ app/src/main/java/com/smy/wordguessgame/HomeFragment.kt
@@ -13,16 +13,32 @@ class HomeFragment : Fragment() {
 
 
     private lateinit var startButton: Button
+    private lateinit var settingsButton: Button
+    private lateinit var quitButton: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        startButton = view.findViewById(R.id.homeStartGameButton)
+
+        view.apply{
+            startButton = findViewById(R.id.homeStartGameButton)
+            settingsButton = findViewById(R.id.homeSettingsButton)
+            quitButton = findViewById(R.id.homeQuitButton)
+        }
+
         startButton.setOnClickListener {
             val action = HomeFragmentDirections.actionHomeFragmentToGameFragment()
             findNavController().navigate(action)
+        }
+
+        settingsButton.setOnClickListener {
+            val action = HomeFragmentDirections.actionGlobalSettingsFragment()
+            findNavController().navigate(action)
+        }
+        quitButton.setOnClickListener {
+            requireActivity().finish()
         }
 
         return view
